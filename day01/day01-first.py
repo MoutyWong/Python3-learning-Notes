b0e2236f49fbbb985d1250e551adef40a992fed4
Mouty Wong <mouty216@gmail.com>
2016-04-12 09:46:30 +0800

    2016/4/12 自觉python3第三天，在GitHub上创建了一个仓库，用来记录自学Python3的一些笔记

---
+++ day01/day01-first.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+print('Hello Python3'),
+print('\n')
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+i = 999
+print('300 + 200 = ,%d',300+200,i)
+#name = input('Please enter your name:')
+#print(name)
+#==================
+#name2 = input('Please enter your name:')
+#print('hello,',name2)
+
+print('1024 * 768 = ',1024*768)
